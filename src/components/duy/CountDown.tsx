67b5329d24df2696405f4fc9f22f836862b30532
Duy Bui <duy.bui@doltech.vn>
2024-09-05 21:51:24 +0700

    add count down

--- src/components/duy/CountDown.tsx
+++ src/components/duy/CountDown.tsx
@@ -7,22 +7,34 @@ const TABS = [
     id: 1,
     label: 'Pomodoro',
     initialValue: '25:00',
+    value: 25 * 60,
   },
   {
     id: 2,
     label: 'Short Break',
     initialValue: '05:00',
+    value: 5 * 60,
   },
   {
     id: 3,
     label: 'Long Break',
     initialValue: '15:00',
+    value: 15 * 60,
   },
 ];
+
+const convertSecondsToTime = (seconds: number) => {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${minutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
+};
 
 export const CountDown = () => {
   const [activeTab, setActiveTab] = React.useState(TABS[0].id);
   const currentActiveTabIndex = TABS.findIndex((tab) => tab.id === activeTab);
+
+  const [currentTime, setCurrentTime] = React.useState(TABS[currentActiveTabIndex].value);
+
   return (
     <div className="mx-auto mt-12 flex max-w-[620px] flex-col justify-center px-20">
       <div className="rounded-md bg-white bg-opacity-10 py-8">
@@ -32,6 +44,7 @@ export const CountDown = () => {
               <TabCountDown
                 onClick={() => {
                   setActiveTab(tabItem.id);
+                  setCurrentTime(tabItem.value);
                 }}
                 active={activeTab === tabItem.id}
                 key={tabItem.id}
@@ -41,10 +54,16 @@ export const CountDown = () => {
           })}
         </div>
         <div className="my-8 text-center text-8xl font-bold">
-          {TABS[currentActiveTabIndex].initialValue}
+          {convertSecondsToTime(currentTime)}
         </div>
         <div className="text-center">
-          <button className="bordered rounded-md border-2 bg-white px-12 py-3 text-2xl font-medium text-red-800 drop-shadow">
+          <button
+            onClick={() => {
+              setCurrentTime(currentTime - 1);
+              // setCurrentTime((prev) => prev - 1);
+            }}
+            className="bordered rounded-md border-2 bg-white px-12 py-3 text-2xl font-medium text-red-800 drop-shadow"
+          >
             START
           </button>
         </div>
