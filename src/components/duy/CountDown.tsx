8ec46938c8dc1f58008d0c52edaf588e3275b14c
Duy Bui <duy.bui@doltech.vn>
2024-09-05 21:33:08 +0700

    update style

--- src/components/duy/CountDown.tsx
+++ src/components/duy/CountDown.tsx
@@ -22,21 +22,32 @@ const TABS = [
 
 export const CountDown = () => {
   const [activeTab, setActiveTab] = React.useState(TABS[0].id);
+  const currentActiveTabIndex = TABS.findIndex((tab) => tab.id === activeTab);
   return (
-    <div className="mx-auto mt-4 max-w-[620px] px-20">
-      <div className="flex justify-center space-x-2 rounded-md bg-white bg-opacity-20 py-4">
-        {TABS.map((tabItem) => {
-          return (
-            <TabCountDown
-              onClick={() => {
-                setActiveTab(tabItem.id);
-              }}
-              active={activeTab === tabItem.id}
-              key={tabItem.id}
-              label={tabItem.label}
-            />
-          );
-        })}
+    <div className="mx-auto mt-4 flex max-w-[620px] flex-col justify-center px-20">
+      <div className="rounded-md bg-white bg-opacity-20 py-4">
+        <div className="flex justify-center space-x-2">
+          {TABS.map((tabItem) => {
+            return (
+              <TabCountDown
+                onClick={() => {
+                  setActiveTab(tabItem.id);
+                }}
+                active={activeTab === tabItem.id}
+                key={tabItem.id}
+                label={tabItem.label}
+              />
+            );
+          })}
+        </div>
+        <div className="my-8 text-center text-8xl font-bold">
+          {TABS[currentActiveTabIndex].initialValue}
+        </div>
+        <div className="text-center">
+          <button className="bordered rounded-md border-2 bg-white px-12 py-3 text-2xl font-medium text-red-800 drop-shadow">
+            START
+          </button>
+        </div>
       </div>
     </div>
   );
