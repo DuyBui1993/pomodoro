a20070fbd0afbabea709db5f9606e39a94c963d8
HoangOanh <vothihoangoanh889@gmail.com>
2024-09-11 21:05:16 +0700

    add TikTok Tab

---
+++ src/components/oanh/tiktok/TikTokHeader.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export const TikTokHeader = () => {
+  return (
+    <div className="flex-1 items-center justify-between p-2">
+      <div className="logo text-4xl">TikTok</div>
+    </div>
+  );
+};
