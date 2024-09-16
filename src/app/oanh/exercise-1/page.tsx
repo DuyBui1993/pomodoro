7c945e31ba744800b853a12ac89c7bea86c48b81
HoangOanh <vothihoangoanh889@gmail.com>
2024-09-16 20:35:13 +0700

    add exercise-1

---
+++ src/app/oanh/exercise-1/page.tsx
@@ -0,0 +1,12 @@
+import { AppleAirPods } from '@/components/oanh/exercise-1/AppleAirPods';
+
+const HomePage = () => {
+  return (
+    <div className="h-screen bg-black">
+      <div className="mx-auto h-[960px] w-[1600px] bg-[#16325B]">
+        <AppleAirPods />
+      </div>
+    </div>
+  );
+};
+export default HomePage;
