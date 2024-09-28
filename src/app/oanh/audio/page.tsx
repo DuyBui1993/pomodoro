8ceda3438a5768665cb26ff14aef050e82bdb97d
HoangOanh <vothihoangoanh889@gmail.com>
2024-09-28 14:20:36 +0700

    add Audio

---
+++ src/app/oanh/audio/page.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Audio } from '@/components/oanh/Audio/Audio';
+
+const HomePage = () => {
+  return (
+    <div className="h-screen bg-white text-black">
+      <div>
+        <Audio />
+      </div>
+    </div>
+  );
+};
+export default HomePage;
